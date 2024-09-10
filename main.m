e13ada3eb0d1432990584cb80904f112f4c00902
Pejmanshojam31 <pejman.shojaee@tu-dresden.de>
2024-09-10 16:29:20 +0200

    Add files via upload

---
+++ main.m
@@ -0,0 +1,18 @@
+clc
+clear
+
+N = 10000; %Number of patients
+%Generate the pathological data as diffusion and prolifration for the 
+% %model:
+% gen_Pat(N);
+% %Generate the simulations for the patients:
+% sim1;
+% %Generate the simulations for the patients:
+% IWandTM(N)
+%Visualizing the results
+% plot_IW_TM(N)
+% TumorFrontSpeed(N)
+% Heatmap()
+
+gen_Pat2(N);
+sim2;
